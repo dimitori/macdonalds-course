8cf70b6d696bf1f59d07586a8f37860208bbe339
nikitaklinchev <nikita.klincehv@mail.ru>
2022-07-16 13:11:17 +0300

    update

--- api/main.py
+++ api/main.py
@@ -1,11 +1,15 @@
 import asyncio
+import sys
+import copy
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
-from cashiers import cashiers, show_cashiers, choose_cashier, CashierNotFree, make_order
-from objects import chefs
+sys.path.append('/Users/nikitaklinchev/PycharmProjects/boot_telegram/macdonalds-course')
+from cashiers import cashiers, show_cashiers, choose_cashier, CashierNotFree, make_order, show_products, products
+from chefs import chefs
+from objects import choosen_products, _calculate_money
+from orders_queue import orders_queue
 
 app = FastAPI()
 
@@ -36,6 +40,10 @@ async def app_startup():
 async def show_cashiers_() -> list[str]:
     return show_cashiers()
 
+@app.get("/show_products")
+async def show_products_() -> list[str]:
+    return show_products()
+
 
 @app.post("/choose_cashier")
 async def choose_cashier_(id_: int):
@@ -48,9 +56,53 @@ async def choose_cashier_(id_: int):
 
 @app.post("/make_order")
 async def make_order_(cashier: Cashier):
-    make_order(cashier.id)
-    return f"Кассир {cashier.id} освободился"
+    cost = _calculate_money(choosen_products)
+    orders_queue.append(make_order(cashier.id, choosen_products, cost))
+    choosen_products.clear()
+    Products = []
+    for i in range(len(orders_queue[-1].products)):
+       Products.append((orders_queue[-1].products[i].name))
 
+    return f"Кассир {cashier.id} освободился, " \
+           f"продукты  {','.join(Products)} в заказе, стоимость заказа {cost}"
+
+@app.post('/choose_cashier')
+async def choose_cashier_(id_:int):
+    cashier = choose_cashier(id_)
+    return f"Кассир {cashier.id} выбраннн"
+
+@app.get('/show_cashiers')
+async def show_all_cashiers():
+    return show_cashiers()
+
+# @app.get('/show_chefs')
+# async def show_all_chefs():
+#     return show_chefs()
+#
+# @app.post('/choose_chefs')
+# async def choose_chefs_(id_:int):
+#     chef = choose_chefs(id_)
+#     return f"Повар {chef.id} выбран, время приготовления {chef.cooking_time} cекунд"
+#
+# @app.post('/choose_chefs')
+# async def choose_chefs_(id_:int):
+#     chef = choose_chefs(id_)
+#     return f"Повар {chef.id} выбран, время приготовления {chef.cooking_time} cекунд"
+
+@app.get('/show_chosen_products')
+async def show_chosen_products():
+    return [f"продукт {c.name} выбран" for c in choosen_products]
+
+@app.post('/choose_products')
+async def choose_products_(number:int):
+    product = products[number-1]
+    choosen_products.append(product)
+    return f"Продукт  {product.name} выбран"
+
+@app.get('/show_orders')
+async def show_orders():
+    return [f"Заказ №{order.id} {order.is_ready} " \
+            f"с продуктами {order.products}" for order in orders_queue]
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=9000, reload=True)
--- orders.py
+++ orders.py
@@ -14,14 +14,13 @@ class Order:
 
     _id: ClassVar[int] = 0
 
-    queue_orders: ClassVar[Queue] = Queue()
 
     def __post_init__(self):
         Order._id += 1
         self.id = Order._id
-        Order.queue_orders.put(self)
         print(f'Order #{self.id} is processed:product {self.products} and cost={self.cost}')
 
     @staticmethod
     def get_order():
         return Order.queue_orders.get()
+
